cf93767bf31e88f28d67642095cd9d154062d64a
Elena Ivanova <elenaxivanova@gmail.com>
2021-01-07 22:46:15 +0300

    Release 1.2: add func delete + animation

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector('.todo-list');
 
 //Event Listeners
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
 
 //Functions
 function addTodo(event) {
@@ -15,7 +16,7 @@ function addTodo(event) {
   todoDiv.classList.add('todo');
   //Create todo LI:
   const newTodo = document.createElement('li');
-  newTodo.innerText = 'hey';
+  newTodo.innerText = todoInput.value;
   newTodo.classList.add('todo-item');
   todoDiv.appendChild(newTodo);
   //Check mark button:
@@ -26,8 +27,29 @@ function addTodo(event) {
   //Check trash button:
   const trashButton = document.createElement('button');
   trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-  trashButton.classList.add("complete-btn");
+  trashButton.classList.add("trash-btn");
   todoDiv.appendChild(trashButton);
   //Append to List:
   todoList.appendChild(todoDiv);
+  //Clear todo input value
+  todoInput.value = '';
+}
+
+function deleteCheck (e) {
+  const item = e.target;
+  //delete todo item
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    //animation
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function() {
+      todo.remove();
+    })
+  }
+
+  //check mark
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
 }
