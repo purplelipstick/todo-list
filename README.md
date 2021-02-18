141a7fb748565b104fd0cdbd03f5671a0e3e6a15
Elena Ivanova <elenaxivanova@gmail.com>
2021-02-18 18:01:44 +0300

    Release 1.3: save/delete todos in Local Storage

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Todo list
 ### Technologys: HTML5, CSS3, JS
 It's my first js project.
+
+Release 1: this is code from https://youtu.be/Ttf3CEsEwMQ 
+Functional:
+- Add/delete todos
+- Mark the task as completed
+- Filter: all/completed/uncompleted
+
--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector('.todo-list');
 const filterOption = document.querySelector(".filter-todo")
 
 //Event Listeners
+document.addEventListener('DOMContentLoaded', getTodos);
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
 filterOption.addEventListener("click", filterTodo);
@@ -21,6 +22,8 @@ function addTodo(event) {
   newTodo.innerText = todoInput.value;
   newTodo.classList.add('todo-item');
   todoDiv.appendChild(newTodo);
+  //Add todo to localStorage
+  saveLocalTodos(todoInput.value);
   //Check mark button:
   const completedButton = document.createElement('button');
   completedButton.innerHTML = '<i class="fas fa-check"></i>';
@@ -44,6 +47,7 @@ function deleteCheck (e) {
     const todo = item.parentElement;
     //animation
     todo.classList.add("fall");
+    removeLocalTodos(todo);
     todo.addEventListener("transitionend", function() {
       todo.remove();
     })
@@ -81,4 +85,63 @@ function filterTodo (e) {
           break;
     }
   });
+}
+
+//Local Storage
+function saveLocalTodos(todo) {
+  let todos;
+  //check: Do I already have thing in there?
+  if(localStorage.getItem('todos') === null){
+    todos = [];
+  }else{
+    todos = JSON.parse(localStorage.getItem('todos'));
+  }
+
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function getTodos(){
+  let todos;
+  //check: Do I already have thing in there?
+  if(localStorage.getItem('todos') === null){
+    todos = [];
+  }else{
+    todos = JSON.parse(localStorage.getItem('todos'));
+  }
+  todos.forEach(function(todo){
+  //Add todo DIV:
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+  //Create todo LI:
+  const newTodo = document.createElement('li');
+  newTodo.innerText = todo;
+  newTodo.classList.add('todo-item');
+  todoDiv.appendChild(newTodo);
+  //Check mark button:
+  const completedButton = document.createElement('button');
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  //Check trash button:
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+  //Append to List:
+  todoList.appendChild(todoDiv);
+  })
+}
+
+function removeLocalTodos(todo){
+  let todos;
+  //check: Do I already have thing in there?
+  if (localStorage.getItem('todos') === null){
+    todos = [];
+  }else{
+    todos = JSON.parse(localStorage.getItem('todos'));
+  }
+  const todoIndex = todo.children[0].innerText;
+  todos.splice(todos.indexOf(todoIndex), 1);
+  localStorage.setItem('todos', JSON.stringify(todos));
 }
